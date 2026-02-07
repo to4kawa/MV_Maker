966d3b28b709a88563b32789e4a71b9739f092e2
toshihisa kawamata <to.4kawa@gmail.com>
2026-02-07 11:55:04 +0900

    Add webpack alias configuration in next.config.js

--- next.config.js
+++ next.config.js
@@ -1,3 +1,14 @@
+const path = require("path");
+
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
-module.exports = nextConfig
+const nextConfig = {
+  webpack: (config) => {
+    config.resolve.alias = {
+      ...(config.resolve.alias || {}),
+      "@": path.resolve(__dirname),
+    };
+    return config;
+  },
+};
+
+module.exports = nextConfig;
