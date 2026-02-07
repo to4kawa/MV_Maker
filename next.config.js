6b58ab616f6fb01662667866e5daf1fc97adfd6a
toshihisa kawamata <to.4kawa@gmail.com>
2026-02-07 14:36:14 +0900

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,3 +1,12 @@
+// next.config.js  （ffmpeg のバイナリ同梱はここで強制する）
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  output: "standalone",
+  experimental: {
+    outputFileTracingIncludes: {
+      "/api/render": ["./node_modules/ffmpeg-static/**", "./node_modules/ffprobe-static/**"]
+    }
+  }
+};
+
 module.exports = nextConfig;
