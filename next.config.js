09083748f4984eb5d5b1e82c1d072b90727c9a71
toshihisa kawamata <to.4kawa@gmail.com>
2026-02-07 13:35:39 +0900

    Include ffmpeg binary in Next.js tracing

--- next.config.js
+++ next.config.js
@@ -2,6 +2,9 @@ const path = require("path");
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  outputFileTracingIncludes: {
+    "/app/api/render/route": ["./node_modules/ffmpeg-static/ffmpeg"],
+  },
   webpack: (config) => {
     config.resolve.alias = {
       ...(config.resolve.alias || {}),
