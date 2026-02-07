cdc217119d56f41fb1d48c0e178039cc59abf12b
toshihisa kawamata <to.4kawa@gmail.com>
2026-02-07 13:56:31 +0900

    Modify ffmpeg-static path and remove webpack alias
    Updated API path for ffmpeg-static inclusion and removed webpack alias configuration.

--- next.config.js
+++ next.config.js
@@ -1,16 +1,7 @@
-const path = require("path");
-
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   outputFileTracingIncludes: {
-    "/app/api/render/route": ["./node_modules/ffmpeg-static/ffmpeg"],
-  },
-  webpack: (config) => {
-    config.resolve.alias = {
-      ...(config.resolve.alias || {}),
-      "@": path.resolve(__dirname),
-    };
-    return config;
+    "/api/render": ["./node_modules/ffmpeg-static/**"],
   },
 };
 
